c9a233a6f98ba0aea9dc42345aed3e25a21b12f1
Fenz <uukooo@126.com>
2017-07-25 21:11:33 +0800

    add amonut for callback payload

--- libs/CallbackPayload.php
+++ libs/CallbackPayload.php
@@ -24,14 +24,16 @@ class CallbackPayload
 	 * @param  string $code
 	 * @param  string $tradeId
 	 * @param  int $time
+	 * @param  int $amount
 	 * @param  int $payerId
 	 * @param  array $extensions
 	 */
-	public function __construct( string$code, string$tradeId, int$time, int$payerId=null, array$extensions=[] )
+	public function __construct( string$code, string$tradeId, int$time, int$amount, int$payerId=null, array$extensions=[] )
 	{
 		$this->details['code']= $code;
 		$this->details['tradeId']= $tradeId;
 		$this->details['time']= $time;
+		$this->details['amount']= $amount;
 		$this->details['payerId']= $payerId;
 		$this->details['extensions']= $extensions;
 	}
